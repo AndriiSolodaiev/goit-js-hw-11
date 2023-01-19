fd16b9280e11bb1eca584e700d5b7e8117889ee3
AndriiSolodaiev <andik05016@gmail.com>
2023-01-19 14:01:08 +0200

    corrected code with async/await

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { Notify } from 'notiflix';
 import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
-import "simplelightbox/dist/simple-lightbox.min.css";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const KEY = '32825730-a8ef2845eee46c8d864ef66d9';
 const API_URL = 'https://pixabay.com/api/';
@@ -9,7 +9,7 @@ const API_URL = 'https://pixabay.com/api/';
 
 const options = {
   root: null,
-  rootMargin: '200px',
+  rootMargin: '150px',
   thresholder: 1.0,
 };
 const guard = document.querySelector('.js-guard');
@@ -19,57 +19,62 @@ let inputValue = '';
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 let lightbox = new SimpleLightbox('.gallery a', {
-        captionsData: "alt",
-        captionDelay: 250
-    });
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 form.addEventListener('submit', onFormSubmit);
 // loadBtn.addEventListener("click", onloadBtnClick)
 
-
-
-function onFormSubmit(evt) {
+async function onFormSubmit(evt) {
   evt.preventDefault();
   gallery.innerHTML = null;
   //  loadBtn.hidden = true;
   inputValue = form.elements.searchQuery.value.trim();
-  fetchImg(inputValue)
-    .then(resp => {
+  page = 1;
+
+  if (inputValue) {
+    try {
+      const resp = await fetchImg(inputValue);
       if (!resp.data.totalHits) {
         Notify.failure(
           'Sorry, there are no images matching your search query. Please try again'
         );
+        observer.unobserve(guard);
         return;
       }
       Notify.success(`Hooray! We found ${resp.data.totalHits} images.`);
       // loadBtn.hidden = false;
 
       createMarkup(resp.data.hits);
-      lightbox.refresh()
+      lightbox.refresh();
       observer.observe(guard);
-      smoothScroll();
-    })
-    .catch(err => console.log(err));
+    } catch (err) {
+      err => console.log(err);
+    }
+  } else {
+    Notify.info('The input is empty.');
+  }
 }
 
 function onInfinityLoad(entries, observer) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      page += 1;
-      fetchImg(inputValue, page)
-        .then(({ data }) => {
-          createMarkup(data.hits);
-          smoothScroll();
-          lightbox.refresh()
-        })
-        .catch(err => {
+  entries.forEach(async entry => {
+    try {
+      if (entry.isIntersecting) {
+        page += 1;
+        const resp = await fetchImg(inputValue, page);
+        createMarkup(resp.data.hits);
+        if (Math.ceil(resp.data.totalHits / 40) === page) {
           Notify.failure(
             "We're sorry, but you've reached the end of search results."
           );
-          console.log(err);
-          page = 1;
           observer.unobserve(guard);
-        });
+        }
+        smoothScroll();
+        lightbox.refresh();
+      }
+    } catch (err) {
+      console.log(err);
     }
   });
 }
@@ -147,6 +152,8 @@ function createMarkup(arr) {
     .join('');
   gallery.insertAdjacentHTML('beforeend', markupGallery);
 }
+
+
 
 function smoothScroll() {
   const { height: cardHeight } = document
